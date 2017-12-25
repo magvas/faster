f9e41e07de553f5839b8be29e0282147042bb217
magvas <info@magvas.de>
2017-12-25 22:49:41 +0100

    - Hitcount added - Countdown added - Final dialog added

--- app.js
+++ app.js
@@ -1,4 +1,73 @@
-function setRandomPosition(params) {
+var hitcount = 0;
+var hitcountelement;
+var countdownelement;
+var timerStarted;
+var countdown;
+var finaldialog;
+var scoreelement;
+const HITCOUNTINITVALUE = 0;
+const COUNTDOWNINITVALUE = 10;
+const COUNTDOWNFINALVALUE = 0;
+
+function init(){
+    hitcountelement = document.getElementById("hitcount");
+    countdownelement = document.getElementById("countdown");
+    finaldialog = document.getElementById("finaldialog");
+    scoreelement = document.getElementById("score");
+    resetHitCount();  
+    resetCountdown();
+    setRandomPosition();
+    timerStarted = false;      
+}
+
+function resetHitCount(){
+    hitcount = HITCOUNTINITVALUE;
+    setHitCountElement(hitcount);
+}
+
+function resetCountdown() {
+    countdown = COUNTDOWNINITVALUE;
+    setCountdownElement(countdown);
+}
+
+function hitSuccess(){
+    if (!timerStarted) {        
+        runTimer();
+    } 
+    setRandomPosition();
+    hitcount++;
+    setHitCountElement(hitcount);
+}
+
+function runTimer() {    
+    timerStarted = true;
+    if (countdown !== COUNTDOWNFINALVALUE) {
+        setTimeout(() => {
+            countdown--;
+            setCountdownElement(countdown);
+            runTimer();
+        }, 1000);    
+    }
+    else{
+        scoreelement.innerText = hitcount + " hits in " + COUNTDOWNINITVALUE + " seconds!";
+        finaldialog.style.display = "block";
+    }        
+}
+
+function afterFinalDialog() {
+    finaldialog.style.display='none'
+    init();
+}
+
+function setHitCountElement(counter){
+    hitcountelement.innerText = counter;
+}
+
+function setCountdownElement(counter){
+    countdownelement.innerText = counter;
+}
+
+function setRandomPosition() {
     var button = document.getElementById("tapMe");
     var leftPos = button.style.left;
     var topPos = button.style.top;
