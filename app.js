3a6501de35750605cbb8d5e8e7c430c33bad986a
magvas <info@magvas.de>
2017-12-30 17:17:19 +0100

    - Added pause/resume function for timer - enhance final dialog

--- app.js
+++ app.js
@@ -9,26 +9,78 @@ var settingsDialog;
 var countdowninitvalue;
 var countdowninit;
 var timer;
+var tapMe;
+var menu;
+var positionbar;
+var closesettingsdialogbutton;
+var applysettingsdialogbutton;
+var closefinaldialogbutton;
+var actualTimerValue;
+var resultseen;
+
 const HITCOUNTINITVALUE = 0;
 const COUNTDOWNDEFAULTVALUE = 60;
 const COUNTDOWNFINALVALUE = 0;
 
 function init(){
-    hitcountelement = document.getElementById("hitcount");
-    countdownelement = document.getElementById("countdown");
-    finaldialog = document.getElementById("finaldialog");
-    scoreelement = document.getElementById("score");
-    settingsDialog = document.getElementById("settingsdialog");
-    countdowninit = document.getElementById("countdowninit");
-
+    bindControls();
+    registerEventHandler();
     if (countdowninitvalue == undefined) {
         countdowninitvalue = COUNTDOWNDEFAULTVALUE;    
     }
     
     resetHitCount();  
     resetCountdown();
+    
     setRandomPosition();    
     timerStarted = false;      
+}
+
+function bindControls() {
+    hitcountelement = document.getElementById("hitcount");
+    countdownelement = document.getElementById("countdown");
+    finaldialog = document.getElementById("finaldialog");
+    scoreelement = document.getElementById("score");
+    settingsDialog = document.getElementById("settingsdialog");
+    countdowninit = document.getElementById("countdowninit");
+    menu = document.getElementById("menu");
+    tapMe = document.getElementById("tapMe");
+    closefinaldialogbutton = document.getElementById("closeFinalDialog");
+    closesettingsdialogbutton = document.getElementById("closesettingsdialogbutton");
+    applysettingsdialogbutton = document.getElementById("applysettingsdialogbutton");
+    positionbar = document.getElementById("positionbar");
+    resultseen = document.getElementById("resultseen");
+}
+
+function registerEventHandler(){
+    
+    menu.onclick = () => {
+        showSettingsDialog();   
+    }    
+    
+    tapMe.onclick = () => {
+        hitSuccess();
+    }
+    
+    closefinaldialogbutton.onclick = () => {
+        closeFinalDialogAndInit();
+    }
+    
+    closesettingsdialogbutton.onclick = () => {        
+        closeSettingsDialog();
+        resumeTimer();
+    }
+    
+    applysettingsdialogbutton.onclick = () => {
+        applySettings();
+        closeSettingsDialog();
+    }
+
+    resultseen.onchange = () => {
+        if (resultseen.checked) {
+            closefinaldialogbutton.disabled = false;
+        }
+    }
 }
 
 function resetHitCount(){
@@ -64,20 +116,47 @@ function runTimer() {
     }        
 }
 
+function resumeTimer() {
+    if (actualTimerValue != undefined) {
+        countdown = actualTimerValue;
+        runTimer();
+    }    
+}
+
+function stopTimer(actualtimer) {
+    if (actualtimer != undefined) {
+        clearTimeout(actualtimer);
+        timerStarted = false;
+    }
+}
+
 function showFinalDialog(count) {
+    resultseen.checked = false;
+    closefinaldialogbutton.disabled = true;
     scoreelement.innerText = count + " hits in " + countdowninitvalue + " seconds!";
     finaldialog.style.display = "block";
 }
 
 function closeFinalDialogAndInit() {
-    finaldialog.style.display = "none";
+    finaldialog.style.display = "none";    
     init();
 }
 
 function closeSettingsDialog() {
-    settingsDialog.style.display = "none";
+    settingsDialog.style.display = "none";    
+}
+
+function applySettings() {
+    clearTimeout(timer);
     countdowninitvalue = countdowninit.value;
     init();
+}
+
+function stopTimer(actualtimer){
+    if (actualtimer != undefined) {
+        clearTimeout(actualtimer);
+        actualTimerValue = countdown;
+    }
 }
 
 function setHitCountElement(counter){
@@ -88,32 +167,40 @@ function setCountdownElement(counter){
     countdownelement.innerText = counter;
 }
 
-function showSettingsDialog(){
-    clearTimeout(timer);
+function showSettingsDialog(){    
+    stopTimer(timer);
     countdowninit.value = countdowninitvalue;
     settingsDialog.style.display = "block";
 }
 
-function setRandomPosition() {
-    var button = document.getElementById("tapMe");
-    var leftPos = button.style.left;
-    var topPos = button.style.top;
+function setRandomPosition() {    
+    var leftPos = tapMe.style.left;
+    var topPos = tapMe.style.top;
 
     var headerHeight = document.getElementById("header").clientHeight;
     var footerHeight = document.getElementById("footer").clientHeight;
 
-    var newLeftPos = getRndInteger(0,document.documentElement.clientWidth - button.clientWidth);
+    var newLeftPos = getRndInteger(0,document.documentElement.clientWidth - tapMe.clientWidth);
 
-    document.getElementById("tapMe").style.left = newLeftPos + "px";
+    tapMe.style.left = newLeftPos + "px";
 
-    var newTopPos = getRndInteger(0,window.innerHeight - headerHeight - footerHeight - button.clientHeight);
+    var newTopPos = getRndInteger(0,window.innerHeight - headerHeight - footerHeight - tapMe.clientHeight);
  
-     document.getElementById("tapMe").style.top = newTopPos + "px";
+    tapMe.style.top = newTopPos + "px";
  
 
-    document.getElementById("status").innerText = "Horizontal: " + newLeftPos + " | Vertical: " + newTopPos;
+    positionbar.innerText = "H: " + newLeftPos + " | V: " + newTopPos;
 }
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
+
+window.addEventListener("orientationchange", function() {
+    setRandomPosition();
+});
+
+window.onload = function() {
+    init();    
+  };
+
